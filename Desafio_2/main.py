7439c8447fa85269a908ffb9fb6c125d1e6284eb
Iván Martínez <ivanmartinez33@outlook.com>
2020-02-24 10:04:40 -0600

    Desafío 2 terminado

---
+++ Desafio_2/main.py
@@ -0,0 +1,24 @@
+def generate_pine(n):
+  bottom_length = 2*n - 1
+
+  # Imprimir pino
+  for i in range(1, bottom_length + 1, 2):
+    spaces_per_side = (bottom_length - i)//2
+    spaces = " " * spaces_per_side
+    symbols = "*" * i
+
+    print(spaces + symbols + spaces)
+  
+  # Imprimir tronco del pino
+  for i in range(0, n - 2):
+    spaces_per_side = (bottom_length - 1)//2
+    spaces = " " * spaces_per_side
+    symbol = "|"
+
+    print(spaces + symbol + spaces)
+
+# Punto de entrada del programa
+if __name__ == '__main__':
+  # Pedirle los datos al usuario
+  n = int(input("Ingresa el alto del árbol: "))
+  generate_pine(n)
